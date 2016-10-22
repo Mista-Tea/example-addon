56bd6c2796d695b119307e64fa033a3a83b5dd47
Mista-Tea <thatguythomas@hotmail.com>
2016-10-21 23:14:57 -0600

    Update sh_test2.lua

--- lua/exampleaddon/shared/sh_test2.lua
+++ lua/exampleaddon/shared/sh_test2.lua
@@ -1,1 +1,1 @@
-print( "sh_test2.lua -- This will print on both the server and on any client that joins the server. This means they CAN see it and can access it" )
+print( "sh_test2.lua -- This file was run on both the server and on any client that joined the server. This means they CAN see it and can access it" )
