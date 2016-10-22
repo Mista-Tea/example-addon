bd04f7b4c9e4ae7aebdae2b1ba86693b1f693065
Mista-Tea <thatguythomas@hotmail.com>
2016-10-21 23:26:48 -0600

    Update cl_test3.lua

--- lua/exampleaddon/client/cl_test3.lua
+++ lua/exampleaddon/client/cl_test3.lua
@@ -1,1 +1,1 @@
-print( "cl_test3.lua -- This files was only run on the client, since the server merely sent it to them rather than actually running it" )
+print( "cl_test3.lua -- This file was only run on the client, since the server merely sent it to them rather than actually running it" )
