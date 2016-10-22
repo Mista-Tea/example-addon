ff26e10f06696536766cb5d38307c0243cd77887
Mista-Tea <thatguythomas@hotmail.com>
2016-10-21 23:11:21 -0600

    Update sv_test1.lua

--- lua/exampleaddon/server/sv_test1.lua
+++ lua/exampleaddon/server/sv_test1.lua
@@ -1,1 +1,1 @@
-print( "This file will only run on the server. The client will never see it, and cannot access it" )
+print( "sv_test1.lua -- This file was only run on the server. The client will never see it, and cannot access it" )
